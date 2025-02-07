76a34df8cac051750117b471276dbedc124e52e2
palak <palak8856@gmail.com>
2025-02-07 14:37:07 +0530

    Initial Commit

---
+++ inventory-management/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AllProductsComponent } from './products/all-products/all-products.component';
+import { CartComponent } from './products/cart/cart.component';
+import { OrdersComponent } from './products/orders/orders.component';
+import { HomeComponent } from './home/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AllProductsComponent,
+    CartComponent,
+    OrdersComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
